bc9096bf037b26ad72e088ed7030ced9167ddbf3
Arsid99 <roma18sidor@gmail.com>
2022-08-21 21:21:57 +0300

    Resubmission of homework from lesson 2, task 3

--- lesson_2/hw_task3.py
+++ lesson_2/hw_task3.py
@@ -1,12 +1,13 @@
-#Entering the variable input speed of the cyclist
-v = (int(input('Enter the cyclists speed in km/h:')))
-#Entering a variable to enter the time spent
-t = (int(float(input('Enter the time spent on the distance in hour:'))))
-#Distance calculation function
-s = (v * t)
-if s >= 100:
-    print('The cyclist covered the distance')
-if s > 0:
-    print('The cyclist is approximately at the mark no: ' + str(s))
-if s < 0:
-    print('The cyclist is moving in the wrong direction')
+v = int(input('Enter the cyclists speed in km/h:'))
+t = int(input('Enter the time spent on the distance in hour:'))
+s = v * t
+
+if v > 0 and s <= 99 and t >= 0:
+    print(f'According to the given data, in {t} hours the cyclist will be near the mark {s}km mark.')
+elif v < 0 and s >= -100 and t >= 0:
+    print(f'According to the specified data, the cyclist will leave the start in'
+    f'a different direction and overcome the distance {s}km mark.')
+elif s >=100:
+    print(f'The cyclist crossed the finish line!')
+else:
+    print('Some data is entered incorrectly.')
